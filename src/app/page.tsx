5e6c9b3c86598016ea24a3c1e5764f349d94c19d
Katy Rosli <katyrosli@gmail.com>
2024-07-16 14:18:49 +0200

    check user sign in or not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import TypewriterTitle from "@/components/ui/TypewriterTitle";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isSignedIn, isLoaded } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (isSignedIn) {
+        router.push("/dashboard");
+      } else {
+        router.push("/sign-in");
+      }
+    }
+  }, [isLoaded, isSignedIn, router]);
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -18,7 +40,8 @@ export default function Home() {
         <div className="mt-8"></div>
 
         <div className="flex justify-center">
-          <Link href="/dashboard">
+          <Link href="/sign-in">
+          {/* </Link><Link href="/dashboard"> */}
             <Button className="bg-sky-400">
               Get Started
               <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
