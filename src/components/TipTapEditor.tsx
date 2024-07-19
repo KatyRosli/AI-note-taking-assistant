f690806bb0ac7d04362cd31b143f5bfcc0d604e0
Katy Rosli <katyrosli@gmail.com>
2024-07-19 10:53:07 +0200

    edit instructions

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -98,7 +98,7 @@ const TipTapEditor = ({ note }: Props) => {
       </div>
       <div className="mt-16 flex justify-center items-center">
       <span className="text-sm text-center italic">
-        Tip: Use the Submit button for AI autocomplete.
+        Tip: Click the Submit button for AI to autocomplete your sentence.
       </span>
       <Button className="bg-teal-400 text-gray-950 hover:bg-violet-500" onClick={handleAutoComplete}>
           Submit
