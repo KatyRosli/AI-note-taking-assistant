804a29b33369ecb8f2c89d7b2b0baab963cfd6bd
Katy Rosli <katyrosli@gmail.com>
2024-07-19 11:03:13 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,8 @@ import { BsLinkedin, BsGithub, BsBehance, BsInstagram } from "react-icons/bs";
 const Footer = () => {
   return (
     <footer className="w-full bottom-0 p-4 mt-16 text-gray-400 flex justify-center">
-      <a className="flex" href="https://github.com/KatyRosli/AI-note-taking-assistant" target="_blank" rel="noopener noreferrer">
-        Open sourced on {""}
-        <div className="text-violet-500 ms-2 me-2">Github{" "}</div>
-        by
+      <a href="https://github.com/KatyRosli/AI-note-taking-assistant" target="_blank" rel="noopener noreferrer">
+        Open sourced on Github by
       </a> 
       <a href="https://www.katyrosli.com" target="_blank" rel="nopener noreferrer"className="text-violet-500 ms-2">
         {" "}
